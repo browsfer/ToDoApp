8a01fb71cdd5fad3112debec9b6c260448555ce3
browsfer <126813633+browsfer@users.noreply.github.com>
2023-08-02 18:21:02 +0200

    calculating work time for each day of the week

--- lib/components/hours_tile.dart
+++ lib/components/hours_tile.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:time_range_picker/time_range_picker.dart';
 
-class HoursTile extends StatelessWidget {
+class HoursTile extends StatefulWidget {
   final String weekDay;
   const HoursTile({super.key, required this.weekDay});
 
+  @override
+  State<HoursTile> createState() => _HoursTileState();
+}
+
+class _HoursTileState extends State<HoursTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,58 +20,17 @@ class HoursTile extends StatelessWidget {
       ),
       margin: const EdgeInsets.only(top: 10),
       padding: const EdgeInsets.all(15),
-      height: MediaQuery.sizeOf(context).height * 0.1,
-      width: MediaQuery.sizeOf(context).width * 1,
+      height: MediaQuery.sizeOf(context).height * 0.09,
+      // width: MediaQuery.sizeOf(context).width * 1,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(weekDay),
+          Text(widget.weekDay),
+          const Spacer(),
+          const Text('Your working time:'),
 
           //Time Range picker
           IconButton(
-            icon: const Icon(Icons.time_to_leave),
-            onPressed: () async {
-              var result = await showTimeRangePicker(
-                context: context,
-                start: TimeOfDay(hour: 6, minute: 0),
-                strokeWidth: 12,
-                handlerRadius: 14,
-                backgroundWidget: Image.asset(
-                  'assets/time_image.png',
-                  width: 200,
-                  height: 200,
-                ),
-                labels: [
-                  ClockLabel.fromTime(
-                      time: const TimeOfDay(hour: 6, minute: 0),
-                      text: "Get up"),
-                  ClockLabel.fromTime(
-                      time: const TimeOfDay(hour: 9, minute: 0),
-                      text: "Coffee time"),
-                  ClockLabel.fromTime(
-                      time: const TimeOfDay(hour: 15, minute: 0),
-                      text: "Afternoon"),
-                  ClockLabel.fromTime(
-                      time: const TimeOfDay(hour: 18, minute: 0),
-                      text: "Time for a beer"),
-                  ClockLabel.fromTime(
-                      time: const TimeOfDay(hour: 22, minute: 0),
-                      text: "Go to Sleep"),
-                  ClockLabel.fromTime(
-                      time: const TimeOfDay(hour: 2, minute: 0),
-                      text: "Go for a pee"),
-                  ClockLabel.fromTime(
-                      time: const TimeOfDay(hour: 12, minute: 0),
-                      text: "Lunchtime!")
-                ],
-                ticksColor: Colors.white,
-                labelOffset: 40,
-                padding: 55,
-                labelStyle: const TextStyle(fontSize: 18, color: Colors.black),
-              );
-              print(result);
-            },
-          ),
+              icon: const Icon(Icons.timelapse_outlined), onPressed: () {}),
         ],
       ),
     );
--- lib/pages/time_sheets_page.dart
+++ lib/pages/time_sheets_page.dart
@@ -1,29 +1,132 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/components/hours_tile.dart';
+import 'package:intl/intl.dart';
+import 'package:time_range_picker/time_range_picker.dart';
 
-class TimeSheetsPage extends StatelessWidget {
+class TimeSheetsPage extends StatefulWidget {
   const TimeSheetsPage({super.key});
 
   @override
+  State<TimeSheetsPage> createState() => _TimeSheetsPageState();
+}
+
+class _TimeSheetsPageState extends State<TimeSheetsPage> {
+  TimeOfDay? startTime;
+  TimeOfDay? endTime;
+  String workHours = '0.0';
+
+  //List of week days
+  Map<String, String> workHoursMap = {
+    'Monday': '0.0',
+    'Tuesday': '0.0',
+    'Wednesday': '0.0',
+    'Thursday': '0.0',
+    'Friday': '0.0',
+    'Saturday': '0.0',
+    'Sunday': '0.0',
+  };
+
+  List<TimeRange> selectedTimeRanges = List.filled(
+    7,
+    TimeRange(
+      startTime: const TimeOfDay(hour: 0, minute: 0),
+      endTime: const TimeOfDay(hour: 0, minute: 0),
+    ),
+  );
+
+  void _selectTimeRange(BuildContext context, int dayIndex) async {
+    TimeRange? result = await showTimeRangePicker(
+      context: context,
+      start: const TimeOfDay(hour: 6, minute: 0),
+      end: const TimeOfDay(hour: 14, minute: 30),
+      strokeWidth: 12,
+      handlerRadius: 14,
+      interval: const Duration(minutes: 30),
+      backgroundWidget: Image.asset(
+        'assets/time_image.png',
+        width: 200,
+        height: 200,
+      ),
+      labels: [
+        ClockLabel.fromTime(
+            time: const TimeOfDay(hour: 6, minute: 0), text: "Get up"),
+        ClockLabel.fromTime(
+            time: const TimeOfDay(hour: 9, minute: 0), text: "Coffee time"),
+        ClockLabel.fromTime(
+            time: const TimeOfDay(hour: 15, minute: 0), text: "Afternoon"),
+        ClockLabel.fromTime(
+            time: const TimeOfDay(hour: 18, minute: 0),
+            text: "Time for a beer"),
+        ClockLabel.fromTime(
+            time: const TimeOfDay(hour: 22, minute: 0), text: "Go to Sleep"),
+        ClockLabel.fromTime(
+            time: const TimeOfDay(hour: 2, minute: 0), text: "Go for a pee"),
+        ClockLabel.fromTime(
+            time: const TimeOfDay(hour: 12, minute: 0), text: "Lunchtime!")
+      ],
+      ticksColor: Colors.white,
+      labelOffset: 40,
+      padding: 55,
+      labelStyle: const TextStyle(fontSize: 18, color: Colors.black),
+    );
+
+    if (result != null) {
+      setState(() {
+        selectedTimeRanges[dayIndex] = result;
+        startTime = result.startTime;
+        endTime = result.endTime;
+        workHours = _calculateWorkHours(result);
+        workHoursMap[getDayOfWeek(dayIndex)] = _calculateWorkHours(result);
+      });
+    }
+  }
+
+  String getDayOfWeek(int dayIndex) {
+    final now = DateTime.now();
+    final firstDayOfWeek = now.subtract(Duration(days: now.weekday - 1));
+    final dayOfWeek = firstDayOfWeek.add(Duration(days: dayIndex));
+    return DateFormat('EEEE').format(dayOfWeek);
+  }
+
+  String _calculateWorkHours(TimeRange? result) {
+    if (result != null) {
+      final startHour = startTime!.hour + startTime!.minute / 60;
+      final endHour = endTime!.hour + endTime!.minute / 60;
+
+      return (endHour - startHour).toStringAsFixed(2);
+    } else {
+      return '0.0';
+    }
+  }
+
+  String _calculateTotalWorkHours() {
+    double totalHours = 0.0;
+    workHoursMap.forEach((day, hours) {
+      totalHours += double.parse(hours);
+    });
+    return totalHours.toStringAsFixed(2);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Column(
-          //Days + time chooser
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: Text('Current week date'),
+            for (int i = 0; i < 7; i++)
+              ElevatedButton(
+                onPressed: () => _selectTimeRange(context, i),
+                child: Text(getDayOfWeek(i)),
+              ),
+            const SizedBox(height: 16),
+            Text(
+              'Total Work Hours: ${_calculateTotalWorkHours()}',
+              style: const TextStyle(fontSize: 20),
             ),
-            HoursTile(weekDay: 'Mon'),
-            HoursTile(weekDay: 'Tue'),
-            HoursTile(weekDay: 'Wed'),
-            HoursTile(weekDay: 'Thu'),
-            HoursTile(weekDay: 'Fri'),
-            HoursTile(weekDay: 'Sat'),
-            HoursTile(weekDay: 'Sun'),
           ],
         ),
-      ],
+      ),
     );
   }
 }
